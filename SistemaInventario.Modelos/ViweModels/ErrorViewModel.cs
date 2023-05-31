5cb1cb11797ce553790ba35611cd50319ee6fa68
Jorge Flores <jorge.flores2020@hotmail.com>
2023-05-31 16:09:23 -0600

    Primer commint - con los proyectos y areas separadas

---
+++ SistemaInventario.Modelos/ViweModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventario.Modelos.ViweModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
